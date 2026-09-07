1e210a72411fdd4010dec223f8e6db564068a078
PowerQueryTrek <bdpaasch@proton.me>
2026-09-07 17:10:09 -0400

    Add files via upload

---
+++ Mod02_Lab.py
@@ -0,0 +1,18 @@
+# Module 2 Lab - Case Study: if...else and while
+# Brian D. Paasch
+# file name = Mod02_Lab.py
+# This Python app that will accept student names and GPAs and test if the student qualifies
+# for either the Dean's List or the Honor Roll.
+
+lname = input("Please enter student's last name (enter 'ZZZ' to quit): ")
+
+while lname.upper() != 'ZZZ':
+    fname = input("Please enter student's first name: ")
+    gpa = float(input("Please enter student's GPA: "))
+
+    if gpa >= 3.5:
+        print(f"{fname} {lname} qualifies for the Dean's List.")
+    elif gpa >= 3.25:
+        print(f"{fname} {lname} qualifies for the Honor Roll.")
+    
+    lname = input("Please enter student's last name (enter 'ZZZ' to quit): ")
